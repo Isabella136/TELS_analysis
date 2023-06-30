4340ebc8f837671a6085e1c3a2763da04505efcf
Nathalie Bonin <nbonin@ufl.edu>
2023-06-30 16:28:53 -0400

    Restrict models to 2-way interactions

--- linear_regression.R
+++ linear_regression.R
@@ -30,6 +30,8 @@ fill_data_frame <- function(df, count, df_organisms_count, organism=FALSE){
         df$SampleType[index] <- 'Bovine'
       else if ((index-1)%/%(count/df_organisms_count) == 1)
         df$SampleType[index] <- 'Human'
+      else if (((index-1)%/%(count/df_organisms_count) == 2) & df_organisms_count > 3)
+        df$SampleType[index] <- 'Mock'
       else
         df$SampleType[index] <- 'Soil'
     }
@@ -62,7 +64,7 @@ fill_data_frame <- function(df, count, df_organisms_count, organism=FALSE){
     arg <- read.csv(
       paste("./TELS_output/sequel-demultiplex.", 
             ".ccs.fastq_deduplicated.fastq_SHORT_amr_diversity.csv", 
-            sep=non_mock_df$Samples[index]), 
+            sep=df$Samples[index]), 
       row.names=NULL)
     df$ARG[index] <- as.integer(arg$Statistics[1])
     
@@ -103,12 +105,18 @@ lmer_coloc <- function(df, organism = FALSE) {
   lmer.coloc_grand_fit <- {
     if (organism) {
       lmer(
-        Unique_Coloc ~ Probe * SampleType * Chemistry * Total_Read_Length + (1 | SampleID),
+        Unique_Coloc ~  
+          Probe + SampleType + Chemistry + Total_Read_Length 
+        + Probe:SampleType + Probe:Chemistry + Probe:Total_Read_Length 
+        + SampleType:Chemistry + SampleType:Total_Read_Length 
+        + Total_Read_Length:Chemistry + (1 | SampleID),
         data=df)
     }
     else {
       lmer(
-        Unique_Coloc ~ Probe * Chemistry * Total_Read_Length + (1 | SampleID),
+        Unique_Coloc ~ 
+          Probe + Chemistry + Total_Read_Length + Probe:Total_Read_Length 
+        + Chemistry:Total_Read_Length + (1 | SampleID),
         data=df)
     }
   }
@@ -120,12 +128,18 @@ lmer_arg <- function(df, organism = FALSE) {
   lmer.arg_grand_fit <- {
     if (organism) {
       lmer(
-        ARG ~ Probe * SampleType * Chemistry * Total_Read_Length + (1 | SampleID),
+        ARG ~ 
+          Probe + SampleType + Chemistry + Total_Read_Length 
+        + Probe:SampleType + Probe:Chemistry + Probe:Total_Read_Length 
+        + SampleType:Chemistry + SampleType:Total_Read_Length 
+        + Total_Read_Length:Chemistry + (1 | SampleID),
         data=df)
     }
     else {
       lmer(
-        ARG ~ Probe * Chemistry * Total_Read_Length + (1 | SampleID),
+        ARG ~ 
+          Probe + Chemistry + Total_Read_Length + Probe:Total_Read_Length 
+        + Chemistry:Total_Read_Length + (1 | SampleID),
         data=df)
     }
   }
@@ -137,12 +151,18 @@ lmer_mge <- function(df, organism = FALSE) {
   lmer.mge_grand_fit <- {
     if (organism) {
       lmer(
-        MGE ~ Probe * SampleType * Chemistry * Total_Read_Length + (1 | SampleID),
+        MGE ~  
+          Probe + SampleType + Chemistry + Total_Read_Length 
+        + Probe:SampleType + Probe:Chemistry + Probe:Total_Read_Length 
+        + SampleType:Chemistry + SampleType:Total_Read_Length 
+        + Total_Read_Length:Chemistry + (1 | SampleID),
         data=df)
     }
     else {
       lmer(
-        MGE ~ Probe * Chemistry * Total_Read_Length + (1 | SampleID),
+        MGE ~ 
+          Probe + Chemistry + Total_Read_Length + Probe:Total_Read_Length 
+        + Chemistry:Total_Read_Length + (1 | SampleID),
         data=df)
     }
   }
@@ -154,7 +174,9 @@ lmer_read <- function(df, organism = FALSE) {
   lmer.read_grand_fit <- {
     if (organism) {
       lmer(
-        Read_Count ~ Probe * SampleType * Chemistry + (1 | SampleID),
+        Read_Count ~  
+          Probe + SampleType + Chemistry + Probe:SampleType 
+        + Probe:Chemistry + SampleType:Chemistry + (1 | SampleID),
         data=df)
     }
     else {
@@ -241,6 +263,11 @@ write_lmer <- function(lmer_function, lmer_filename, lmer_org_info,
   title(ylab = "Fitted Values", cex.lab = 1, line = 4.5)
   par(op)
   dev.off()
+  data = data.frame(row.names = paste(lmer_org_info, lmer_filename),
+                    'AIC' = extractAIC(lmer)[2],
+                    'edf' = extractAIC(lmer)[1])
+  browser()
+  write.table(data, file=paste(output_folder, 'AIC.csv'), append=TRUE)
   return(lmer.summary[["AICtab"]][["REML"]])
 }
 
@@ -288,6 +315,37 @@ non_mock_df=data.frame(
   ARG = integer(non_mock_count),
   MGE = integer(non_mock_count)
 )
+
+all_count <- 72
+all_df_organisms <- 4
+all_df=data.frame(
+  Samples=c(
+    "BFV2AA","BFV2AB","BFV2AC","BFV2AMA","BFV2AMB","BFV2AMC","BFV2MA","BFV2MB","BFV2MC",
+    "BFXTAA","BFXTAB","BFXTAC","BFXTAMA","BFXTAMB","BFXTAMC","BFXTMA","BFXTMB","BFXTMC",
+    "HFV2AA","HFV2AB","HFV2AC","HFV2AMA","HFV2AMB","HFV2AMC","HFV2MA","HFV2MB","HFV2MC",
+    "HFXTAA","HFXTAB","HFXTAC","HFXTAMA","HFXTAMB","HFXTAMC","HFXTMA","HFXTMB","HFXTMC",
+    "MOV2AA","MOV2AB","MOV2AC","MOV2AMA","MOV2AMB","MOV2AMC","MOV2MA","MOV2MB","MOV2MC",
+    "MOXTAA","MOXTAB","MOXTAC","MOXTAMA","MOXTAMB","MOXTAMC","MOXTMA","MOXTMB","MOXTMC",
+    "SV2AA","SV2AB","SV2AC","SV2AMA","SV2AMB","SV2AMC","SV2MA","SV2MB","SV2MC",
+    "SXTAA","SXTAB","SXTAC","SXTAMA","SXTAMB","SXTAMC","SXTMA","SXTMB","SXTMC"),
+  SampleID=c(
+    "BFV2A","BFV2A","BFV2A","BFV2AM","BFV2AM","BFV2AM","BFV2M","BFV2M","BFV2M",
+    "BFXTA","BFXTA","BFXTA","BFXTAM","BFXTAM","BFXTAM","BFXTM","BFXTM","BFXTM",
+    "HFV2A","HFV2A","HFV2A","HFV2AM","HFV2AM","HFV2AM","HFV2M","HFV2M","HFV2M",
+    "HFXTA","HFXTA","HFXTA","HFXTAM","HFXTAM","HFXTAM","HFXTM","HFXTM","HFXTM",
+    "MOV2A","MOV2A","MOV2A","MOV2AM","MOV2AM","MOV2AM","MOV2M","MOV2M","MOV2M",
+    "MOXTA","MOXTA","MOXTA","MOXTAM","MOXTAM","MOXTAM","MOXTM","MOXTM","MOXTM",
+    "SV2A","SV2A","SV2A","SV2AM","SV2AM","SV2AM","SV2M","SV2M","SV2M",
+    "SXTA","SXTA","SXTA","SXTAM","SXTAM","SXTAM","SXTM","SXTM","SXTM"),
+  Chemistry = integer(all_count),
+  SampleType = integer(all_count),
+  Probe = integer(all_count),
+  Read_Count = integer(all_count),
+  Total_Read_Length = integer(all_count),
+  Unique_Coloc = integer(all_count),
+  ARG = integer(all_count),
+  MGE = integer(all_count)
+)
               
 non_mock_df <- fill_data_frame(
   non_mock_df, non_mock_count, non_mock_df_organisms, TRUE)
@@ -295,7 +353,10 @@ non_mock_df <- fill_data_frame(
 mock_df <- fill_data_frame(
   mock_df, mock_count, mock_df_organisms)
 
-output_folder <- './output/regression_results/'
+all_df <- fill_data_frame(
+  all_df, all_count, all_df_organisms, TRUE)
+
+output_folder <- './output/regression_results/restricted/'
 
 if (file.exists(output_folder) == FALSE)
   dir.create(output_folder)
@@ -303,17 +364,23 @@ if (file.exists(output_folder) == FALSE)
 lmer_function_list <- c(lmer_coloc, lmer_arg, lmer_mge, lmer_read)
 plot_function_list <- c(plot_coloc, plot_arg, plot_mge, plot_read)
 lmer_filename_list <- c("coloc_", "arg_", "mge_", "read_counts_")
-lmer_org_info_list <- c("mock_", "nonmock_")
-df_list <- list(mock_df, non_mock_df)
+lmer_org_info_list <- c("re_mock_", "re_nonmock_", "re_all_")
+df_list <- list(mock_df, non_mock_df, all_df)
 
 summary <- list()
+name <- list()
 
 for (i in 1:4) {
-  for (j in 1:2) {
+  for (j in 1:3) {
     summary <- append(summary, write_lmer(
       lmer_function_list[[i]], lmer_filename_list[i], lmer_org_info_list[j], 
-      plot_function_list[[i]], df_list[[j]], j == 2))
+      plot_function_list[[i]], df_list[[j]], j >= 2))
+    name <- append(name, paste(lmer_org_info_list[j], lmer_filename_list[i]))
   }
 }
+df.reml <- data_frame(
+  row.names = array(unlist(name)),
+  'REML' = array(unlist(summary))
+)
 
-
+write.csv(df.reml, file=paste(output_folder, 're_REML.csv'))
